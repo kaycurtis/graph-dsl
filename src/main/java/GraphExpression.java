39b61babe96055311d5f7d6c60e43838bd6e7f2a
MagY <yangm110615@gmail.com>
2019-11-11 12:49:27 -0800

    test

--- src/main/java/GraphExpression.java
+++ src/main/java/GraphExpression.java
@@ -1,3 +1,23 @@
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 public class GraphExpression {
     //TODO populate this
+
+
+    // test push
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        // from graphstream tutorial page
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+    }
 }
